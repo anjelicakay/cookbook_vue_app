92e190fa164116f01380d7621aaff65e33e7a902
anjelicakay <aktowner@gmail.com>
2019-02-25 13:20:40 -0600

    added recipes table

--- src/router.js
+++ src/router.js
@@ -4,6 +4,10 @@ import RecipesIndex from './views/RecipesIndex.vue'
 import RecipesNew from './views/RecipesNew.vue'
 import RecipesShow from './views/RecipesShow.vue'
 import RecipesEdit from './views/RecipesEdit.vue'
+import Signup from "./views/Signup.vue"
+import Login from "./views/Login.vue"
+import Logout from "./views/Logout.vue"
+import RecipesTable from "./views/RecipesTable.vue"
 
 Vue.use(Router)
 
@@ -11,9 +15,13 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {path: '/', name: 'home', component: RecipesIndex},
-    {path: '/recipes/new', name: 'recipes-new', component: RecipesNew},
-    {path: '/recipes/:id', name: 'recipes-show', component: RecipesShow},
-    {path: '/recipes/:id/edit', name: 'recipes-edit', component: RecipesEdit}
+    { path: '/', name: 'home', component: RecipesIndex },
+    { path: '/recipes/new', name: 'recipes-new', component: RecipesNew },
+    { path: '/recipes/:id', name: 'recipes-show', component: RecipesShow },
+    { path: '/recipes/:id/edit', name: 'recipes-edit', component: RecipesEdit },
+    { path: "/signup", name: "signup", component: Signup },
+    { path: "/login", name: "login", component: Login },
+    { path: "/logout", name: "logout", component: Logout },
+    { path: "/table", name: "table", component: RecipesTable}
   ]
 })
---
+++ src/views/RecipesTable.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="recipes-index">
+    <h1>All Recipes</h1>
+    <div>
+      Filter Title: <input v-model="titleFilter" list="titles">
+
+      <datalist id="titles">
+        <option v-for="recipe in recipes">{{ recipe.title }}</option>
+      </datalist>
+    </div>
+
+    <table class="table table-striped table-dark">
+      <thead class="thead-light">
+        <tr>
+          <th scope="col" v-on:click="setSortAttribute('id')">ID {{ orderIndicator('id') }}</th>
+          <th scope="col" v-on:click="setSortAttribute('title')">Title  {{ orderIndicator('title') }}</th> 
+          <th scope="col" v-on:click="setSortAttribute('chef')">Chef {{ orderIndicator('chef') }}</th>
+          <th scope="col" v-on:click="setSortAttribute('prep_time')">Prep Time {{ orderIndicator('prep_time') }}</th>
+        </tr>
+      </thead>
+      <tbody is="transition-group" name="slide-left">
+        <tr v-for="recipe in orderBy(filterBy(recipes, titleFilter, 'title'), sortAttribute, sortOrder)" v-bind:key="recipe.id">
+          <th scope="row">
+            {{ recipe.id }}
+          </th>
+          <td>
+            <router-link v-bind:to="'/recipes/' + recipe.id">
+              {{ recipe.title }}
+            </router-link>
+          </td>
+          <td>
+            {{ recipe.chef }}
+          </td>
+          <td>
+            {{ recipe.formatted.prep_time }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+import Vue2Filters from "vue2-filters";
+
+export default {
+  data: function() {
+    return {
+      recipes: [],
+      currentRecipe: {},
+      titleFilter: '',
+      sortAttribute: 'title',
+      sortOrder: 1
+    };
+  },
+  created: function() {
+    axios.get("/api/recipes")
+      .then(response => {
+        this.recipes = response.data;
+      });
+  },
+  methods: {
+    setSortAttribute: function(inputAttribute) {
+      if (this.sortAttribute === inputAttribute) {
+        this.sortOrder *= -1;
+      } else {
+        this.sortAttribute = inputAttribute;
+        this.sortOrder = 1;
+      }
+    },
+    orderIndicator: function(inputAttribute) {
+      if (this.sortAttribute === inputAttribute) {
+        if (this.sortOrder === 1) {
+          return "▼";
+        } else {
+          return "▲";
+        }
+      } else {
+        return " ";
+      }
+    }
+  },
+  mixins: [Vue2Filters.mixin]
+};
+</script>
